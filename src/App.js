ab4680c2e1391811c1c7c55a02f51f68690a0b24
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-09 18:24:09 +0530

    final look with no functionalities

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
 <h1>Hello World!</h1>
+<input></input>
+<button>Add todo</button>
+<ul>
+  <li>Take dogs</li>
+  <li>Take to shop</li>
+</ul>
     </div>
   );
 }
