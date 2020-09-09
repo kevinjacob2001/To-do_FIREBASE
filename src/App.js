072a6b92b0b9a48d1cea1f2e07e89b4ccc7123fe
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-09 18:35:36 +0530

    added useState()

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+const [todos,setTodos]=useState(["Take dog for walk","go to beach"])
+
   return (
     <div className="App">
-<h1>Hello World!</h1>
-<input></input>
-<button>Add todo</button>
-<ul>
-  <li>Take dogs</li>
-  <li>Take to shop</li>
-</ul>
+    
+          <h1>Hello World!</h1>
+          <input></input>
+          <button>Add todo</button>
+         
+          <ul>
+            
+            {todos.map((todo)=>{
+              return(
+                <li>{todo}</li>
+              )
+            })}
+
+          </ul>
+    
     </div>
   );
 }
