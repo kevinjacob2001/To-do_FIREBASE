7cdfce33cff98760102952439c55152b6542c492
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-09 19:32:33 +0530

    added material ui form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Button} from '@material-ui/core'
+import {Button, FormControl,InputLabel,Input} from '@material-ui/core'
 import './App.css';
 
 function App() {
@@ -18,15 +18,17 @@ const addTodo=(event)=>{
     <div className="App">
     
           <h1>Hello World!</h1>
-          
           <form onSubmit={addTodo}>
-          <input value={input} onChange={event=>setInput(event.target.value)}/>
+   
 
-      <Button type="submit" onClick={addTodo} variant="contained" color="primary">
-      Add todo
-      </Button>
-              
-          
+          <FormControl>
+            <InputLabel>Write a todo</InputLabel>
+            <Input  value={input} onChange={event=>setInput(event.target.value)}/>
+          </FormControl>
+
+            <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+            Add todo
+            </Button>
           </form>
 
           <ul>
