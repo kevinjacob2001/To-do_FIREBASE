93401efe6601d9807c08ece1f4a418efba1818a4
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-09 18:55:55 +0530

    Added input field

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-const [todos,setTodos]=useState(["Take dog for walk","go to beach"])
+const [todos,setTodos]=useState(["Take dog for walk","go to beach","Welcome Kevin"])
+const [input,setInput]=useState("")
+console.log(input);
+
 
   return (
     <div className="App">
     
           <h1>Hello World!</h1>
-          <input></input>
+          <input value={input} onChange={event=>setInput(event.target.value)}/>
           <button>Add todo</button>
          
           <ul>
-            
             {todos.map((todo)=>{
               return(
                 <li>{todo}</li>
               )
             })}
-
           </ul>
     
     </div>
