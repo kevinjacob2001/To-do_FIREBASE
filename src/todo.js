db0b233f29be42be8f1f069175dab77e23344880
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-10 13:53:30 +0530

    adding delete button

--- src/todo.js
+++ src/todo.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { ListItemText,ListItem,List,ListItemAvatar } from '@material-ui/core';
+import { ListItemText,ListItem,List,ListItemAvatar,Button } from '@material-ui/core';
 import './Todo.css'
+import db from './firebase';
 
 function Todo(props){
     return(
-        <List className="todo__list"> 
+       <div>
+       <List className="todo__list"> 
            <ListItem>
                <ListItemAvatar>
  
                </ListItemAvatar>
                <ListItemText primary={props.todo.todo} secondary="Dummy deadline"/>
            </ListItem>
+           <Button onClick={()=>db.collection("todo").doc(props.todo.id).delete()} variant="contained" color="secondary">DELETE ME</Button>
         </List>
+    <hr/>
+    </div>
     )
 }
 
