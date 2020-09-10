e9af14f340880081ad684c2e8992ab9faf96ee1d
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-10 12:27:32 +0530

    added timestamp()

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Button, FormControl,InputLabel,Input} from '@material-ui/core'
 import './App.css';
 import Todo from './Todo';
 import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
 const [todos,setTodos]=useState([ ])
@@ -11,7 +12,7 @@ const [input,setInput]=useState("")
 //when app loads we need to listen to db and fetch new todos as they get add or removed
 
 useEffect(()=>{
-db.collection("todos").onSnapshot(snapshot=>{
+db.collection("todos").orderBy("timestamp","desc").onSnapshot(snapshot=>{
   //console.log(snapshot.docs.map(doc=>doc.data().todo));
   setTodos(snapshot.docs.map(doc=>doc.data().todo))
 })
@@ -20,7 +21,11 @@ db.collection("todos").onSnapshot(snapshot=>{
 const addTodo=(event)=>{
 
     event.preventDefault();
-    setTodos([...todos,input])
+    db.collection("todos").add({
+todo:input,
+timestamp:firebase.firestore.FieldValue.serverTimestamp()
+    })
+
     setInput("") //clear up the input
 }
 
