ee421909ed8f4381bf077e6663cf8774f3eb2af6
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-10 14:35:32 +0530

    added useStyles

--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ListItemText,ListItem,List,ListItemAvatar,Button } from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import { Modal } from '@material-ui/core';
 import './Todo.css'
 import db from './firebase';
 
-function Todo(props){
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 
+function Todo(props){
+   
+const classes=useStyles();    
+const [open,setOpen]=useState(false)
+
+const handleClose=()=>{
+setOpen(false)
+}
+
+const handleOpen=()=>{
+setOpen(true)
+}  
 
 
     return(
@@ -14,8 +35,13 @@ function Todo(props){
        
        <Modal
        open={open}
-       onClose={handleClose}
-       ></Modal>
+       onClose={()=>setOpen(false)}
+       >
+           <div>
+               <h1>I am a modal</h1>
+               <button onClick={handleClose}>Click here to close</button>
+           </div>
+       </Modal>
 
        <List className="todo__list"> 
            <ListItem>
@@ -24,9 +50,11 @@ function Todo(props){
                </ListItemAvatar>
                <ListItemText primary={props.todo.todo} secondary="Dummy deadline"/>
            </ListItem>
+           <button onClick={()=>setOpen(true)}>EDIT</button>
            <Button onClick={event=>db.collection("todos").doc(props.todo.id).delete()} variant="contained" color="secondary"><DeleteForeverIcon/></Button>
         </List>
     <hr/>
+
     </>
     )
 }
